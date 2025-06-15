f5052d2e61c95d795ec4ed1bcb56295fbaf9dfbc
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-15 09:04:47 +0700

    Adjust the data access with the raw data

--- src/component/countryList.jsx
+++ src/component/countryList.jsx
@@ -1,19 +1,20 @@
 export function CountryList({ country }) {
-  const { name, flag, population, region, capital } = country;
+  const { name, flags, population, region, capital } = country;
   const listCapital = capital.join();
+  const formattedPopulation = population.toLocaleString();
   return (
     <div>
       <div>
         <img
-          src={flag}
+          src={flags.png}
           alt={`${name} Flag`}
           style={{ height: "75px", width: "150px" }}
         />
       </div>
-      <h1>{name}</h1>
+      <h1>{name.common}</h1>
       <div>
         <h2>Population: </h2>
-        <span>{population}</span>
+        <span>{formattedPopulation}</span>
       </div>
       <div>
         <h2>Region: </h2>
