a56624fee009df1fc56fe0cdd9758fe0480b33da
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-19 09:06:27 +0700

    Create custom hook to search country by code

---
+++ src/hooks/useCodeCountry.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { APIFILTER } from "../enum/enum.js";
+
+export function useCodeCountry(code) {
+  const [country, setCountry] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const FIELDS = APIFILTER;
+  const URL = `https://restcountries.com/v3.1/alpha/${code}?fields=${FIELDS}`;
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error");
+        }
+        return response.json();
+      })
+      .then((response) => setCountry(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [URL]);
+
+  return { country, loading, error };
+}
