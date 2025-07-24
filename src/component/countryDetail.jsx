5cd31f069661040a2d4c3b939c89395cf6d58048
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-24 09:39:19 +0700

    Style and position the back countryDetail btn

--- src/component/countryDetail.jsx
+++ src/component/countryDetail.jsx
@@ -1,5 +1,7 @@
+import { useContext } from "react";
 import BorderCountryBtn from "./borderCountryBtn.jsx";
 import { CountryDetailInfo } from "./countryDetailInfo.jsx";
+import { IsDarkContext } from "../context/themeContext.js";
 
 export function CountryDetail({ country, setCountryDetail, setShowDetail }) {
   const {
@@ -14,6 +16,8 @@ export function CountryDetail({ country, setCountryDetail, setShowDetail }) {
     borders,
     flags,
   } = country;
+
+  const isDark = useContext(IsDarkContext);
 
   let currenciesList = [];
 
@@ -38,22 +42,34 @@ export function CountryDetail({ country, setCountryDetail, setShowDetail }) {
   const languageList = getLanguageList(languages);
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className="h-screen">
+      <div className="px-16 py-16">
+        <button
+          onClick={() => setShowDetail(false)}
+          className={`flex items-center px-8 py-2 rounded-md gap-4 ${
+            isDark
+              ? "bg-(--Dark-Mode-Elements) shadow-2xl text-(--Dark-Mode-Text-Light-Mode)"
+              : "bg-(--Light-Mode-Background) shadow-sm text-(--Light-Mode-Text)"
+          }`}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 32 32"
             height={15}
             width={15}
+            className={`${
+              isDark
+                ? "fill-(--Dark-Mode-Text-Light-Mode)"
+                : "fill-(--Light-Mode-Text)"
+            }`}
           >
             <path
               d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z"
               data-name="4-Arrow Left"
             />
           </svg>
-          <button onClick={() => setShowDetail(false)}>Back</button>
-        </div>
+          <p className="text-inherit">Back</p>
+        </button>
       </div>
       <div>
         <div>
